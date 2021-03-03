4273867752088e191cebc3b1b79b3ef1f6cc143f
Bruce Collie <brucecollie82@gmail.com>
2021-03-03 17:13:04 +0000

    Sketch out API endpoints
    This app will need four endpoints - one to submit PDF links via POST,
    one to list all submitted PDFs with links to their downloads, and
    endpoints to download PDFs / thumbnails from index entries.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.post('/submit', (req, res) => {});
+
+app.get('/list', (req, res) => {});
+
+app.get('/image/:id', (req, res) => {});
+
+app.get('/pdf/:id', (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`Example listening on port: ${port}`);
+  console.log(`PDF submission app listening on port: ${port}`);
 });
