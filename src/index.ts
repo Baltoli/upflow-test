eaec400cf1cceadef5c1ca504566db1017f0f862
Bruce Collie <brucecollie82@gmail.com>
2021-03-03 17:02:44 +0000

    Convert Hello, World to Express
    Minimal setup to make sure that Express & TS are working properly

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world!');
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Example listening on port: ${port}`);
+});
